e03f7fc6a8d6ce6a10883ca1bbfdcb3398b29aac
Ethnolatin <duchesne.fred@gmail.com>
2020-12-01 16:33:47 +0100

    sauvegarde des "like/dislike" dans la bdd

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -59,13 +59,10 @@ exports.getAllSauces = (req, res, next) => {
 exports.likeSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then(sauce => {
-      console.log(sauce)
-      const likedSauce = likesManagement(sauce, req.body.userId, req.body.like);
-      Sauce.updateOne(sauce, likedSauce)
-        .then(() => {res.status(200).json({ message: "Avis enregistré !" });
-        console.log(sauce)}
-        )
+      likesManagement(sauce, req.body.userId, req.body.like);
+      sauce.save()
+        .then(() => res.status(200).json({ message: "Vote enregistré !" }))
         .catch(error => res.status(400).json({ error }))
-    })
+    })    
     .catch(error => res.status(500).json({ error }));
 }
--- backend/likesManagement.js
+++ backend/likesManagement.js
@@ -1,23 +1,23 @@
 module.exports = (sauce, userId, like) => {
   switch (like) {
     case 1:
-        sauce.likes += 1;
-        sauce.usersLiked.push(userId);
+      sauce.usersLiked.push(userId);
+      sauce.likes++;
       break;
     case -1:
-        sauce.dislikes += 1;
-        sauce.usersDisliked.push(userId);
+      sauce.usersDisliked.push(userId);
+      sauce.dislikes++;
       break;
     case 0:
       const likeIndex = sauce.usersLiked.indexOf(userId)      
-      const dislikeIndex = sauce.usersDisliked.indexOf(userId)      
-      if ( likeIndex !== -1) {
-        sauce.likes -= 1;
+      if (likeIndex !== -1) {
         sauce.usersLiked.splice(likeIndex, 1);
+        sauce.likes--;
       };
+      const dislikeIndex = sauce.usersDisliked.indexOf(userId)      
       if (dislikeIndex !== -1) {
-        sauce.dislikes -= 1;
         sauce.usersDisliked.splice(dislikeIndex, 1);
+        sauce.dislikes--;
       };
       break;
     default:
